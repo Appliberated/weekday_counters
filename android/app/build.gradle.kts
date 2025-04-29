53960a2b9e54f89813ed3aafaa01cf16033d0c3f
TechAurelian <dev@techaurelian.com>
2025-04-29 18:03:38 +0300

    Add Android launcher icon

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -8,7 +8,8 @@ plugins {
 android {
     namespace = "com.tecdrop.weekday_counters"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    // ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
